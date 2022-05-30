8ea47fd93110fbd302dd9581a382777fef31dfa5
Allison Bohannon <akboh.github@gmail.com>
2022-05-29 19:44:50 -0700

    commit tree 975e696aa640ff3d7fa2b4e46403b9e712054049

--- index.js
+++ index.js
@@ -9,6 +9,52 @@
  for you to use if you need it!
  */
 
+ function createEmployeeRecord(arr) {
+    return {
+        firstName: arr[0],
+        familyName: arr[1], 
+        title: arr[2], 
+        payPerHour: arr[3], 
+        timeInEvents: [], 
+        timeOutEvents: [],
+    }
+}; 
+
+function createEmployeeRecords(arr) {
+    const recordsArray = arr.map(record => createEmployeeRecord(record))
+    return recordsArray; 
+}; 
+
+function createTimeInEvent(dateStamp) {
+    this.timeInEvents = [...this.timeInEvents, {
+        type: 'TimeIn', 
+        hour: parseInt(dateStamp.slice(11)),
+        date: dateStamp.slice(0,10),
+    }]
+    return this; 
+}; 
+
+function createTimeOutEvent(dateStamp) {
+    this.timeOutEvents = [...this.timeOutEvents, {
+        type: 'TimeOut', 
+        hour: parseInt(dateStamp.slice(11)),
+        date: dateStamp.slice(0,10),
+    }]
+    return this; 
+}; 
+
+function hoursWorkedOnDate(dateStamp) {
+    console.log(this)
+    let startEvent = this.timeInEvents.find(event => event.date === dateStamp)
+    let endEvent = this.timeOutEvents.find(event => event.date === dateStamp)
+    return (endEvent.hour - startEvent.hour)/100;   
+}; 
+
+function wagesEarnedOnDate(dateStamp) {
+    const hours = hoursWorkedOnDate.call(this, dateStamp);
+    return hours * this.payPerHour; 
+};
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -20,4 +66,12 @@ const allWagesFor = function () {
 
     return payable
 }
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(record => record.firstName === firstName)
+}; 
+
+function calculatePayroll(arr) {
+    return arr.reduce((prevValue, currentValue) => prevValue + allWagesFor.call(currentValue), 0)
+}; 
 
